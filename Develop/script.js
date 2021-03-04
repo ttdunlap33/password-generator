1bfd042c8699c661b4c3d5d51d5ac4848e73ea2a
Thomas Dunlap <ttdunlap33@gmail.com>
2021-03-04 16:29:55 -0700

    pushing changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,84 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function generatePassword() {
- var length = prompt("How many chjaracters do you want your password to be? Please enter between 8-128 characters.");
- 
 
-  // var password = generatePassword();
-  if (length < 8 || length > 128){
+function generatePassword() {
+  var length = prompt("How many chjaracters do you want your password to be? Please enter between 8-128 characters.");
+
+
+
+  if (length < 8 || length > 128) {
     alert("Password length must be between 8-128 characters.");
     return;
   }
 
-var upper = confirm("Would you like to include uppercase characters?");
- var lower = confirm("Would you like to include lowercase characters?");
- var special = confirm("Would you like your password to include special characters?");
- var numbers = confirm("Would you like your password to include numbers?");
+  var upper = confirm("Would you like to include uppercase characters?");
+  var lower = confirm("Would you like to include lowercase characters?");
+  var special = confirm("Would you like your password to include special characters?");
+  var numbers = confirm("Would you like your password to include numbers?");
 
- if (upper === false && lower === false && special === false && numbers === false){
-   alert("One of the four character types must be selected");
-   return;
- }
+  if (upper === false && lower === false && special === false && numbers === false) {
+    alert("One of the four character types must be selected");
+    return;
+  }
 
   var passwordText = document.querySelector("#password");
-  
-var i;
-var options = [];
+
+  var i;
+  var options = [];
+
+
+
+  if (upper === true) {
+    var upperArray = [];
+
+    for (i = 65; i <= 90; i++) {
+      upperArray.push(String.fromCharCode(i));
+    }
+    options = options.concat(upperArray);
+
+  } if (lower === true) {
+    var lowerArray = [];
+
+    for (i = 97; i <= 122; i++) {
+      lowerArray.push(String.fromCharCode(i));
+    }
+    options = options.concat(lowerArray);
+  }
+  var passsword = "";
+
+  if (special === true) {
+    var specialString = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+
+    options = options.concat(specialString.split(""));
+  }
+
+  if (numbers === true) {
+    var numberArray = [];
+    for (i = 0; i <= 9; i++) {
+      numberArray.push(i)
+    } options = options.concat(numberArray);
+  }
 
 
 
 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+  for (i = 0; i < length; i++) {
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index];
+    passsword = passsword + computerChoice;
+  }
+
+
+  console.log(upperArray)
+  console.log(lowerArray)
+  console.log(specialString)
+  console.log(numberArray)
+  console.log(options)
+
+  passwordText.value = passsword;
+
+}
 
 
